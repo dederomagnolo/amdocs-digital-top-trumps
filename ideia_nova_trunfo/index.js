8cf0c996b5fdff912a9e185047259c158ab0f736
Bruno Lucas <bruno.dossantos@amdocs.com>
2020-01-08 18:11:43 -0300

    New idea, removed react. Kept html+css+javascript

---
+++ ideia_nova_trunfo/index.js
@@ -0,0 +1,287 @@
+window.onload = function() {
+  // Constructor Function for a single card
+  function createCard(name, image_src, data, id) {
+    this.name = name;
+    this.img = image_src;
+    this.data = data;
+    this.id = id;
+  }
+
+  // Constructor Function for an empty pack
+  function createPack(name) {
+    this.name = name;
+    this.cards = [];
+
+    // Method to add a new card to the pack
+    this.add_new_card = function(name, image_src, data) {
+      var num_cards = this.cards.length;
+      var new_card = new createCard(name, image_src, data, num_cards + 1);
+      this.cards.push(new_card);
+    };
+
+    // method to shuffle the pack of cards
+    // Fisher--Yates Algorithm -- https://bost.ocks.org/mike/shuffle/
+    this.shuffle = function() {
+      var m = this.cards.length;
+      var temp, index;
+      // While there remain elements to shuffle…
+      while (m > 0) {
+        // decrease m by 1
+        m--;
+        // Pick a remaining element…
+        index = Math.floor(Math.random() * m);
+
+        // And swap it with the current element.
+        temp = this.cards[m];
+        this.cards[m] = this.cards[index];
+        this.cards[index] = temp;
+      }
+    };
+
+    // Method to put the top card to the bottom
+    this.top_to_bottom = function() {
+      this.cards.push(this.cards[0]);
+      // remove the first card
+      this.cards.splice(0, 1);
+    };
+  }
+
+  // Create the Game Object which contains properties and methods for the state of the overall game
+  function createGame() {
+    this.player_pack = [];
+    this.player_pack[0] = new createPack("player1_cards");
+    this.player_pack[1] = new createPack("player2_cards");
+    this.current_player = 0;
+    // method to random deal a pack of cards into two piles
+    this.deal_pack = function(pack) {
+      // shuffle the input pack
+      pack.shuffle();
+      // empty the cards from each pack
+      this.player_pack[0].cards.length = 0;
+      this.player_pack[1].cards.length = 0;
+
+      var num_cards = pack.cards.length;
+      var num_each = Math.ceil(num_cards / 2);
+
+      // add first half of the cards to the first player and the second half of the cards to the second player
+      this.player_pack[0].cards = pack.cards.slice(0, num_each); //slice the pack starting with 0 and ending with num_each
+      this.player_pack[1].cards = pack.cards.slice(num_each); //slice the pack from num_each to end
+
+      // set the current player to be the first player
+      this.current_player = 0;
+    };
+    // method gives top cards to the winning player
+    this.win = function(win_player) {
+      var lose_player = 1 - win_player;
+      var lost_card = this.player_pack[lose_player].cards[0];
+
+      this.player_pack[win_player].top_to_bottom(); // winning player's top card goes to the bottom
+      this.player_pack[win_player].cards.push(lost_card); // add losing card to that of the winning player
+      this.player_pack[lose_player].cards.splice(0, 1); // delete the losing card from the losing player
+
+      if (this.player_pack[lose_player].cards.length === 0) {
+        $(".gameover").css("visibility", "visible");
+      }
+
+      //this.current_player = win_player; // change the current_player to be the winning player
+    };
+    // method shows the current state of the game
+    this.show_game = function() {
+      // find the elements for the cards
+      var $card_areas = $(".card");
+
+      // Add the class "selected" to current player"s and flipped to the non-player's card.
+      $card_areas.removeClass("selected flipped"); //remove classes from both cards first
+      $card_areas.eq(this.current_player).addClass("selected");
+      $card_areas.eq(1 - this.current_player).addClass("flipped");
+
+      // loop through the player packs
+      for (var player = 0; player < this.player_pack.length; player++) {
+        var $player_card_area = $card_areas.eq(player); // DOM element for this player"s cards
+
+        if (this.player_pack[player].cards.length > 0) {
+          var top_card = this.player_pack[player].cards[0]; //top card of this player"s deck
+          var i = 0; // index number
+          var $data_area = $player_card_area.find("td.data");
+
+          // Assign the relevant details of the cards to their place on the DOM
+          $player_card_area.find(".card_name").text(top_card.name); // name of card
+          $player_card_area.find("img.card_picture").attr("src", top_card.img); // attribute the image source
+          $player_card_area
+            .find(".num_cards")
+            .text(this.player_pack[player].cards.length); // nuber of cards in this player pback
+          //loop through each of the data area elements
+          $data_area.each(function() {
+            $(this).text(top_card.data[i]);
+            i++;
+          });
+
+          $player_card_area.css("visibility", "visible"); // if the player has cards make the cards visible
+        } else {
+          $player_card_area.css("visibility", "hidden"); // if the player has no cards hide the cards
+        }
+      }
+    };
+    // method compares two data values for a particular index and returns the winning player
+    this.compare_data = function(i) {
+      var win_player;
+      if (
+        this.player_pack[0].cards[0].data[i] >
+        this.player_pack[1].cards[0].data[i]
+      ) {
+        win_player = 0; // First player wins
+      } else {
+        win_player = 1; // Second player wins
+      }
+      return win_player;
+    };
+    // method for the transition animation putting cards to the bottom of the winning hand
+    this.transition_animation = function() {
+      var shift_direction = "200px";
+      if (this.current_player === 0) {
+        shift_direction = "-200px";
+      }
+      var $winner_card = $(".card").eq(this.current_player);
+      var $loser_card = $(".card").eq(1 - this.current_player);
+
+      var pos_winner = $winner_card.position();
+      var pos_loser = $loser_card.position();
+      var left_shift = pos_winner.left - pos_loser.left;
+
+      // adds the CSS class "flipped" which causes the cards to transition to their flipped side.
+      $(".card").addClass("flipped");
+
+      $(".front, .back")
+        .animate({ left: shift_direction }, "slow") // moves
+        .animate({ zIndex: "-5" }, "fast")
+        .animate({ left: "0px" }, "fast")
+        .animate({ zIndex: "2" }, "fast", function() {
+          $(".front, .back").clearQueue(); // clear any queued animations
+          $("tr").removeClass("winner loser");
+          game.show_game();
+        });
+    };
+    // method for the animation comparing the two cards
+    this.compare_animation = function(i) {
+      var winner = this.current_player;
+      var loser = 1 - this.current_player;
+      var $winner_card = $(".card").eq(winner);
+      var $loser_card = $(".card").eq(loser);
+      $winner_card
+        .find("tr")
+        .eq(i)
+        .addClass("winner");
+      $loser_card
+        .find("tr")
+        .eq(i)
+        .addClass("loser");
+      $(".card").removeClass("flipped selected");
+      $winner_card.addClass("selected");
+      $(".front").one("transitionend", function() {
+        game.transition_animation();
+      });
+    };
+  }
+
+  // Create a new pack
+  var FullPack = new createPack("Programming Languages");
+  var game = new createGame();
+
+  // Add cards to the pack
+  FullPack.add_new_card(
+    "PHP",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/php-logo_1.png",
+    [1995, 7.0, 9, 6]
+  );
+  FullPack.add_new_card(
+    "CSS",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/css3-logo.png",
+    [1996, 3, 6, 4]
+  );
+  FullPack.add_new_card(
+    "HTML",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/html5-logo.png",
+    [1993, 5.1, 3, 6]
+  );
+  FullPack.add_new_card(
+    "jQuery",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/jquery-logo.png",
+    [2006, 2.1, 6, 5]
+  );
+  FullPack.add_new_card(
+    "Python",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/python-logo.png",
+    [1991, 3.5, 6, 9]
+  );
+  FullPack.add_new_card(
+    "Sass",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sass-logo.png",
+    [2006, 3.4, 5, 2]
+  );
+  FullPack.add_new_card(
+    "BBC BASIC",
+    "https://upload.wikimedia.org/wikipedia/commons/0/0e/Hard_reset_BBC_Micro_32K_Acorn_DFS.gif",
+    [1981, 5.9, 2, 2]
+  );
+  FullPack.add_new_card(
+    "Matlab",
+    "http://www.mathworks.com/cmsimages/64848_wl_cc_logo_membrane_2002_wl.gif",
+    [1984, 8.6, 5, 8]
+  );
+  FullPack.add_new_card(
+    "Scratch",
+    "https://pbs.twimg.com/profile_images/57362682/cat_400x400.jpg",
+    [2002, 2.0, 1, 2]
+  );
+  FullPack.add_new_card(
+    "JavaScript",
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/js-logo.png",
+    [1995, 1.8, 7, 8]
+  );
+
+  // Shuffle and deal the cards to two players
+  game.deal_pack(FullPack);
+  // Show the game - display the top cards in the DOM
+  game.show_game();
+
+  // Callback function for the "next" button
+  // Moves the top card to the bottow and shows the new top card.
+  $("#button_next").on("click", function() {
+    game.player_pack[0].top_to_bottom();
+    game.player_pack[1].top_to_bottom();
+    game.show_game();
+  });
+
+  // Callback function for the "shuffle" button
+  // Shuffles (randomly permutes) the order of the card.
+  $("#button_shuffle").on("click", function() {
+    game.deal_pack(FullPack);
+    game.show_game();
+    $(".gameover").css("visibility", "hidden");
+  });
+
+  // Triggers the event that the clicked player wins.
+  $(".button_win").on("click", function() {
+    var player = $(this).data("player"); // determine the player
+    game.win(player);
+    game.compare_animation(player);
+    game.transition_animation(2);
+  });
+
+  // Callback for selecting a answer
+  $(".card").on("click", "tr", function() {
+    if ($(this).parents(".selected").length === 0) {
+      return;
+    } // KLUDGE quit the function if the ancestor card was not selected
+    var win_player = game.compare_data($(this).index());
+    game.win(win_player);
+    game.compare_animation($(this).index());
+    //  game.transition_animation();
+    //  game.show_game();
+  });
+
+  // Callback for transition
+  $(".button_transition").on("click", function() {
+    game.compare_animation(2);
+  });
+};
